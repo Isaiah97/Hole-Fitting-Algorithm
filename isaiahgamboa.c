de8f8e88dd71b74f0405025d41a11b4cc992faaa
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-23 17:07:37 -0800

    figuring out more logic. on break rn

--- isaiahgamboa.c
+++ isaiahgamboa.c
@@ -28,7 +28,58 @@ void print_menu(void);
 void print_allocated_blocks(void);
 
 void enter_parameters(void);
-void allocated_first_fit(void);
+void allocated_first_fit(void){
+	int id, block_size;
+
+	if (pm_size <= 0) {
+		printf("Error: Physical memory size not set. Choose option 1 first.\n");
+		return;
+	}
+
+	printf("Enter block id: ");
+	if (scanf("%d", &id) != 1) {
+		int c;
+		while ((c = getchar()) != '\n' && c != EOF) { }
+			printf("Invalid id.\n");
+		return;
+	}
+
+	printf("Enter block size: ");
+	if (scanf("%d", &block_size) != 1 || block_size <= 0){
+		int c;
+		while ((c = getchar()) != '\n' && c != EOF) { }
+			printf("Invalid block size.\n");
+		return;
+	}
+
+	if (id_exists(id)) {
+		printf("Error: Duplicate block id. Allocation rejected. \n");
+		return
+	}
+
+	if (head == NULL) {
+		if (block_size > pm_size) {
+			printf("Error: Not enough memory to allocate block.\n");
+			return;
+		}
+
+		Block *new_block = (Block *)malloc(sizeof(Block));
+		if (!new_block) {
+			printf("Error: Memory allocation failed.\n");
+			return;
+		}
+
+		new_block->id = id;
+		new_block->start = 0;
+		new_block->end = block_size;
+		new_block->next = NULL;
+
+		head = new_block;
+		return;
+	}
+
+	
+}
 void allocated_best_fit(void);
 void deallocate_block(void);
 void defragment_memory(void);
